bf0fc13fc9988de2f2412befcd224ea7073c4c33
urancon <ulysse.rancon@gmail.com>
2025-05-13 10:03:23 +0200

    add NPLL loss

---
+++ deepSTRF/metrics/losses.py
@@ -0,0 +1,37 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+
+
+class NegativePoissonLogLikelihood(nn.Module):
+    """
+    Negative Poisson Log-Likelihood loss.
+
+    See for instance:
+      -  Singer et al. (2023) Hierarchical temporal prediction captures motion processing along the visual pathway.
+        eLife, https://doi.org/10.7554/eLife.52599.
+      -  Wang et al. (2025) Foundation model of neural activity predicts response to new stimulus types.
+        Nature, https://www.nature.com/articles/s41586-025-08829-y.
+
+    """
+    def __init__(self, reduction='mean'):
+        super(NegativePoissonLogLikelihood, self).__init__()
+        self.reduction = reduction
+
+    def forward(self, prediction, psth):
+        # both input tensors should be positive and of shape (B, N, T)
+        assert not (prediction < 0.).any() and not (psth < 0.).any()
+        assert prediction.shape == psth.shape
+
+        L = - torch.mean((psth * torch.log(-prediction + 1e-9) - prediction), dim=(-2, -1))
+        if self.reduction == "mean":
+            L = L.mean(0)
+        elif self.reduction == "sum":
+            L = L.sum()
+        elif self.reduction == 'None':
+            pass
+        else:
+            raise NotImplementedError
+
+        return L
