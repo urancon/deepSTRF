5383096727b1a1d5ec09217b450645ec0562f1aa
urancon <ulysse.rancon@gmail.com>
2025-09-26 11:13:47 +0200

    added logo to readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = "_static/deepstrf_logo.png"
 
 
 from recommonmark.parser import CommonMarkParser
